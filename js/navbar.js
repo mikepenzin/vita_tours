3c4d1e26fdbe8c5f0caf8ff1c72989863ebf3336
mikepenzin <mike.penzin@gmail.com>
2016-10-03 17:02:00 +0300

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -10,8 +10,13 @@
             // set distance user needs to scroll before we fadeIn navbar
 			if ($(this).scrollTop() > 100) {
 				$('.navbar-default').css('background-color', '#393e41').fadeIn();
+				
+				$('.collapsing').css('background-color', '#393e41').fadeIn();
+
 			} else {
-				$('.navbar-default').css('background-color', 'transparent');;
+				$('.navbar-default').css('background-color', 'transparent');
+			
+				$('.collapsing').css('background-color', 'transparent');
 			}
 		});
 	
